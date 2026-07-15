87220a5d43c9c24d97ec46704608e22b95024334
Nabeel <nabeelmehmood448@gmail.com>
2026-07-15 19:41:04 +0500

    configure Shopping OAuth in test bootstrap

--- aimatic/install.py
+++ aimatic/install.py
@@ -1,3 +1,7 @@
 def before_tests():
 	"""Load ERPNext's standard test master data before Aimatic integration tests."""
 	import erpnext.tests.utils
+
+	from aimatic.patches.create_shopping_oauth_client import execute as create_shopping_oauth_client
+
+	create_shopping_oauth_client()
